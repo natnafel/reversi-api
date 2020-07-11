b7ba771a040f98f997b1b8ef22f3a84e8e74f5b8
Natnael Negash <natnael.feleke@gmail.com>
2020-07-11 00:30:26 -0500

    #11 fix JPA mapping bug CellLocation not mapped to table when for Move to be saved

--- src/main/java/com/cs525/reversi/models/Move.java
+++ src/main/java/com/cs525/reversi/models/Move.java
@@ -2,12 +2,7 @@ package com.cs525.reversi.models;
 
 import java.util.List;
 
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 import com.cs525.reversi.req.CellLocation;
 
