f671d0173cf0126d7a533114485f8259921bc9b7
Mostafa Mosad <mustafamosad57@gmail.com>
2020-07-14 17:46:18 -0500

    add interceptor to debug request and response

--- src/main/java/com/cs525/reversi/ReversiApplication.java
+++ src/main/java/com/cs525/reversi/ReversiApplication.java
@@ -1,38 +1,51 @@
 package com.cs525.reversi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import com.cs525.reversi.util.away.LoggingRequestInterceptor;
+
 @SpringBootApplication
 @EnableWebMvc
 @EnableScheduling
 public class ReversiApplication extends WebMvcConfigurerAdapter {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ReversiApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ReversiApplication.class, args);
+	}
 
-    //for DTOs
-    @Bean
+	// for DTOs
+	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
 
 	@Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+	public RestTemplate restTemplate() {
+		RestTemplate restTemplate = new RestTemplate(
+				new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()));
+		List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+		interceptors.add(new LoggingRequestInterceptor());
+		restTemplate.setInterceptors(interceptors);
+		return restTemplate;
+	}
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
-    }
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**");
+	}
 
 }
